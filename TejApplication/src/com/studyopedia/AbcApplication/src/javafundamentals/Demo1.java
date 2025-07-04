ee4676047420408bcc43abc59f76ae6562c181e8
Tejaswini <jagadamtejaswini77@gmail.com>
2025-07-04 15:36:47 +0530

    java

---
+++ TejApplication/src/com/studyopedia/AbcApplication/src/javafundamentals/Demo1.java
@@ -0,0 +1,14 @@
+package javafundamentals;
+import java.util.*;
+public class Demo1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		String t=sc.next();
+		System.out.println(s + "Technologies" + t);
+		sc.close();
+
+	}
+
+}
